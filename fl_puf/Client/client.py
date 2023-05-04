68fa55076273975046e1b69284c8204fff2eaeba
lucacorbucci <corbuccilu@gmail.com>
2023-05-04 15:56:00 +0200

    🚧 Working on the computation of the violation and on the use of regularization during the training

---
+++ fl_puf/Client/client.py
@@ -0,0 +1,177 @@
+from pathlib import Path
+
+import flwr as fl
+import ray
+import torch
+from fl_puf.Utils.dataset_utils import DatasetDownloader
+from fl_puf.Utils.model_utils import Learning
+from fl_puf.Utils.utils import Utils
+from flwr.common.typing import Scalar
+from opacus.validators import ModuleValidator
+from torch import nn
+
+
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(
+        self,
+        cid: str,
+        fed_dir_data: str,
+        dataset_name: str,
+        DPL: bool,
+        DPL_lambda: float,
+        private: bool,
+        epsilon: float,
+        clipping: float,
+        epochs: int,
+        delta: float,
+        lr: float,
+    ):
+        self.cid = cid
+        self.fed_dir = Path(fed_dir_data)
+        self.properties: dict[str, Scalar] = {"tensor_type": "numpy.ndarray"}
+
+        # Instantiate model
+        self.lr = lr
+        self.net = Utils.get_model(dataset_name)
+        self.optimizer = torch.optim.SGD(self.net.parameters(), lr=self.lr)
+        self.optimizer_regularization = None
+        self.model_regularization = None
+        self.private = private
+        self.is_private_model_initialized = False
+        self.epsilon = epsilon
+        self.clipping = clipping
+
+        self.criterion = nn.CrossEntropyLoss()
+
+        self.DPL = DPL
+        self.DPL_lambda = DPL_lambda
+
+        # Determine device
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.dataset_name = dataset_name
+
+    def get_parameters(self, config):
+        return Utils.get_params(self.net)
+
+    def fit(self, parameters, config):
+        Utils.set_params(self.net, parameters)
+
+        # Load data for this client and get trainloader
+        num_workers = int(ray.get_runtime_context().get_assigned_resources()["CPU"])
+        train_loader = DatasetDownloader.get_dataloader(
+            self.fed_dir,
+            self.cid,
+            is_train=True,
+            batch_size=config["batch_size"],
+            workers=num_workers,
+            dataset=self.dataset_name,
+        )
+        test_loader = DatasetDownloader.get_dataloader(
+            self.fed_dir,
+            self.cid,
+            is_train=False,
+            batch_size=config["batch_size"],
+            workers=num_workers,
+            dataset=self.dataset_name,
+        )
+
+        if self.private and not self.is_private_model_initialized:
+            self.net, self.optimizer, strain_loader = Learning.create_private_model(
+                model=self.net,
+                epsilon=self.epsilon,
+                optimizer=self.optimizer,
+                train_loader=train_loader,
+                epochs=self.epochs,
+                delta=self.delta,
+                MAX_GRAD_NORM=self.clipping,
+            )
+
+            if self.DPL:
+                # If we want to use DPL with private training
+                # we have to create a second model because
+                # we can't just sum the two losses.
+                self.model_regularization = Utils.get_model(
+                    self.dataset_name,
+                    self.device,
+                )
+                self.model_regularization = ModuleValidator.fix(
+                    self.model_regularization,
+                )
+
+                self.optimizer_regularization = torch.optim.SGD(
+                    self.model_regularization.parameters(),
+                    lr=self.lr,
+                )
+
+                (
+                    self.model_regularization,
+                    self.optimizer_regularization,
+                    _,
+                ) = Learning.create_private_model(
+                    model=self.model_regularization,
+                    epsilon=self.epsilon,
+                    optimizer=self.optimizer_regularization,
+                    train_loader=train_loader,
+                    epochs=self.epochs,
+                    delta=self.delta,
+                    MAX_GRAD_NORM=self.gradnorm,
+                )
+                self.model_regularization.to(self.device)
+
+        # Send model to device
+        self.net.to(self.device)
+
+        Learning.train_loop(
+            epochs=config["epochs"],
+            model=self.net,
+            model_regularization=self.model_regularization,
+            optimizer=self.optimizer,
+            optimizer_regularization=self.optimizer_regularization,
+            train_loader=train_loader,
+            test_loader=test_loader,
+            device=self.device,
+            private=self.private,
+            criterion=self.criterion,
+            DPL=self.DPL,
+            DPL_lambda=self.DPL_lambda,
+        )
+
+        # Return local model and statistics
+        return Utils.get_params(self.net), len(train_loader.dataset), {}
+
+    def evaluate(self, parameters, config):
+        Utils.set_params(self.net, parameters)
+
+        # Load data for this client and get trainloader
+        num_workers = int(ray.get_runtime_context().get_assigned_resources()["CPU"])
+
+        valloader = DatasetDownloader.get_dataloader(
+            self.fed_dir,
+            self.cid,
+            is_train=False,
+            batch_size=config["batch_size"],
+            workers=num_workers,
+            dataset=self.dataset_name,
+        )
+
+        # Send model to device
+        self.net.to(self.device)
+
+        (
+            loss,
+            accuracy,
+            max_disparity_test,
+        ) = Learning.test(
+            self.net,
+            valloader,
+            device=self.device,
+            epoch=0,
+            DPL_lambda=self.DPL_lambda,
+            max_disparity_computation=False,
+        )
+
+        # Evaluate
+        # loss, accuracy = Utils.test(self.net, valloader, device=self.device)
+
+        # Return statistics
+        return float(loss), len(valloader.dataset), {"accuracy": float(accuracy)}
--- fl_puf/main.py
+++ fl_puf/main.py
@@ -3,15 +3,17 @@ from typing import Dict
 
 import flwr as fl
 import wandb
-from client import FlowerClient
-from dataset_utils import DatasetDownloader
 from flwr.common.typing import Scalar
-from model_utils import Learning
-from utils import Utils
+
+from fl_puf.Client.client import FlowerClient
+from fl_puf.Utils.dataset_utils import DatasetDownloader
+from fl_puf.Utils.model_utils import Learning
+from fl_puf.Utils.utils import Utils
 
 parser = argparse.ArgumentParser(description="Flower Simulation with PyTorch")
 
 parser.add_argument("--num_client_cpus", type=int, default=1)
+parser.add_argument("--num_client_gpus", type=int, default=1)
 parser.add_argument("--num_rounds", type=int, default=5)
 parser.add_argument("--dataset", type=str, default=None)
 parser.add_argument("--epochs", type=int, default=1)
@@ -19,6 +21,20 @@ parser.add_argument("--batch_size", type=int, default=64)
 parser.add_argument("--pool_size", type=int, default=100)
 parser.add_argument("--sampled_clients", type=float, default=0.1)
 parser.add_argument("--wandb", type=bool, default=False)
+parser.add_argument("--DPL", type=bool, default=False)
+parser.add_argument("--DPL_lambda", type=float, default=0.0)
+parser.add_argument("--private", type=bool, default=False)
+parser.add_argument("--epsilon", type=float, default=10)
+parser.add_argument("--clipping", type=float, default=10)
+parser.add_argument("--delta", type=float, default="0.1")
+parser.add_argument("--lr", type=float, default="0.1")
+
+
+# DPL:
+# 1) baseline without privacy and DPL -> compute maximum violation
+# 2) baseline without privacy and with DPL -> compute maximum violation
+# 3) baseline with privacy and without DPL -> compute maximum violation
+# 4) baseline with privacy and with DPL -> compute maximum violation
 
 
 def setup_wandb(args):
@@ -54,7 +70,10 @@ if __name__ == "__main__":
     dataset_name = args.dataset
 
     pool_size = args.pool_size
-    client_resources = {"num_cpus": args.num_client_cpus, "num_gpus": 1}
+    client_resources = {
+        "num_cpus": args.num_client_cpus,
+        "num_gpus": args.num_client_gpus,
+    }
 
     if args.wandb:
         wandb_run = setup_wandb(args)
@@ -73,40 +92,54 @@ if __name__ == "__main__":
         pool_size=pool_size,
         alpha=float("inf"),
         num_classes=2,
-        val_ratio=0.1,
+        val_ratio=0.2,
     )
     print(pool_size)
 
-    # # configure the strategy
-    # strategy = fl.server.strategy.FedAvg(
-    #     fraction_fit=args.sampled_clients,
-    #     fraction_evaluate=args.sampled_clients,
-    #     min_fit_clients=1,
-    #     min_evaluate_clients=1,
-    #     min_available_clients=args.sampled_clients,  # All clients should be available
-    #     on_fit_config_fn=fit_config,
-    #     evaluate_fn=Learning.get_evaluate_fn(
-    #         testset,
-    #         dataset_name,
-    #         wandb_run,
-    #     ),  # centralised evaluation of global model
-    # )
+    # configure the strategy
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=args.sampled_clients,
+        fraction_evaluate=args.sampled_clients,
+        min_fit_clients=1,
+        min_evaluate_clients=1,
+        min_available_clients=args.sampled_clients,  # All clients should be available
+        on_fit_config_fn=fit_config,
+        evaluate_fn=Learning.get_evaluate_fn(
+            testset,
+            dataset_name,
+            wandb_run,
+        ),  # centralised evaluation of global model
+    )
 
-    # def client_fn(cid: str):
-    #     # create a single client instance
-    #     return FlowerClient(cid, fed_dir, dataset_name)
+    def client_fn(cid: str):
+        # create a single client instance
+        return FlowerClient(
+            cid,
+            fed_dir,
+            dataset_name,
+            DPL=args.DPL,
+            DPL_lambda=args.DPL_lambda,
+            private=args.private,
+            epsilon=args.epsilon,
+            clipping=args.clipping,
+            epochs=args.epochs,
+            delta=args.delta,
+            lr=args.lr,
+        )
 
-    # # (optional) specify Ray config
-    # ray_init_args = {"include_dashboard": False}
+    # (optional) specify Ray config
+    ray_init_args = {"include_dashboard": False}
 
-    # # start simulation
-    # fl.simulation.start_simulation(
-    #     client_fn=client_fn,
-    #     num_clients=pool_size,
-    #     client_resources=client_resources,
-    #     config=fl.server.ServerConfig(num_rounds=args.num_rounds),
-    #     strategy=strategy,
-    #     ray_init_args=ray_init_args,
-    # )
-    # if wandb_run:
-    #     wandb_run.finish()
+    # start simulation
+    fl.simulation.start_simulation(
+        client_fn=client_fn,
+        num_clients=pool_size,
+        client_resources=client_resources,
+        config=fl.server.ServerConfig(
+            num_rounds=args.num_rounds,
+        ),
+        strategy=strategy,
+        ray_init_args=ray_init_args,
+    )
+    if wandb_run:
+        wandb_run.finish()
