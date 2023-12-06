1944fe963773808fb88d674f6f04d8c742fc3a68
Luca Corbucci <lucorbucci@gmail.com>
2023-12-06 15:09:25 +0000

    new: experiments with celeba

--- fl_puf/experiments/celeba/run_sweep_2.sh
+++ fl_puf/experiments/celeba/run_sweep_2.sh
@@ -3,7 +3,6 @@ PROJECT_NAME="FL_PUF_Sweep" # swap out globally
 run_sweep_and_agent () {
   # Set the SWEEP_NAME variable
   SWEEP_NAME="$1"
-
   
   # Run the wandb sweep command and store the output in a temporary file
   poetry run wandb sweep --project "$PROJECT_NAME" --name "$SWEEP_NAME" "$SWEEP_NAME.yaml" >temp_output.txt 2>&1
@@ -12,14 +11,19 @@ run_sweep_and_agent () {
   SWEEP_ID=$(awk '/wandb agent/{ match($0, /wandb agent (.+)/, arr); print arr[1]; }' temp_output.txt)
 
   # Remove the temporary output file
-  rm temp_output.txt
+#   rm temp_output.txt
   
   # Run the wandb agent command
-  poetry run wandb agent $SWEEP_ID --project "$PROJECT_NAME" --count 20
+  poetry run wandb agent $SWEEP_ID --project "$PROJECT_NAME" --count 40
 }
 
-run_sweep_and_agent "baseline_private"
-# run_sweep_and_agent "fixed"
-# run_sweep_and_agent "tunable"
-# run_sweep_and_agent "02_fixed"
-# run_sweep_and_agent "02_tunable"
+run_sweep_and_agent "tunable_private"
+run_sweep_and_agent "fixed_private"
+run_sweep_and_agent "tunable_private"
+run_sweep_and_agent "fixed_private"
+run_sweep_and_agent "tunable_private"
+run_sweep_and_agent "fixed_private"
+run_sweep_and_agent "tunable_private"
+run_sweep_and_agent "fixed_private"
+run_sweep_and_agent "tunable_private"
+run_sweep_and_agent "fixed_private"
