8709b16cf7c894d26b03ffc44b69324ac5fbd1bc
Luca Corbucci <lucorbucci@gmail.com>
2023-12-24 14:09:27 +0000

    Updated version

--- fl_puf/experiments_paper/Celeba/run.sh
+++ fl_puf/experiments_paper/Celeba/run.sh
@@ -2,30 +2,30 @@ cd celeba_baseline_5
 sh run_sweep.sh
 cd ../
 
-cd celeba_baseline_8
-sh run_sweep.sh
-cd ../
+# cd celeba_baseline_8
+# sh run_sweep.sh
+# cd ../
 
-cd celeba_01_epsilon_5
-sh run_sweep.sh
-cd ../
+# cd celeba_01_epsilon_5
+# sh run_sweep.sh
+# cd ../
 
-cd celeba_01_epsilon_8
-sh run_sweep.sh
-cd ../
+# cd celeba_01_epsilon_8
+# sh run_sweep.sh
+# cd ../
 
-cd celeba_005_epsilon_5
-sh run_sweep.sh
-cd ../
+# cd celeba_005_epsilon_5
+# sh run_sweep.sh
+# cd ../
 
-cd celeba_005_epsilon_8
-sh run_sweep.sh
-cd ../
+# cd celeba_005_epsilon_8
+# sh run_sweep.sh
+# cd ../
 
-cd celeba_0075_epsilon_5
-sh run_sweep.sh
-cd ../
+# cd celeba_0075_epsilon_5
+# sh run_sweep.sh
+# cd ../
 
-cd celeba_0075_epsilon_8
-sh run_sweep.sh
-cd ../
+# cd celeba_0075_epsilon_8
+# sh run_sweep.sh
+# cd ../
