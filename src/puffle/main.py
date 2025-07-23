f9c304ac8b4ad7546adef393103eaef4cae1d252
lucacorbucci <corbuccilu@gmail.com>
2025-07-23 17:00:03 +0200

    ✨ Added the code to run the centralised experiment

--- src/puffle/main.py
+++ src/puffle/main.py
@@ -1,17 +1,14 @@
 import argparse
-import logging
 import os
 import random
 import warnings
 
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import tqdm
+import wandb
 from opacus import PrivacyEngine
-from torch import nn
+from torch import nn, optim
 from torch.utils.data import Dataset
 
 from puffle.PUFFLEModel.puffle_model import PUFFLEModel
@@ -186,7 +183,7 @@ class DisparityRegularizationLoss(nn.Module):
         mask = torch.full((fairness_violations.shape[0],), 0, dtype=torch.float32).to(device)
         mask[index] = 1
         mask = mask.to(device)
-        fairness_violations= fairness_violations.to(device)
+        fairness_violations = fairness_violations.to(device)
         res = torch.sum(mask * fairness_violations)
 
         if global_computation:
@@ -461,7 +458,7 @@ class TabularDataset(Dataset):
         return x_sample, z_sample, y_sample, self.indexes[idx], idx
 
 
-def prepare_dutch(base_path):
+def prepare_dutch(base_path, sweep):
     tmp = load_dutch(dataset_path=base_path)
     tmp = dataset_to_numpy(*tmp, num_sensitive_features=1)
 
@@ -481,6 +478,26 @@ def prepare_dutch(base_path):
     z_train = np.array(z[:train_size])
     z_test = np.array(z[train_size:])
 
+    if sweep:
+        val_size = int(len(x_train) * 0.2)
+
+        x_val = np.array(x_train[-val_size:])
+        x_train = np.array(x_train[:-val_size])
+
+        y_val = np.array(y_train[-val_size:])
+        y_train = np.array(y_train[:-val_size])
+
+        z_val = np.array(z_train[-val_size:])
+        z_train = np.array(z_train[:-val_size])
+
+        val_dataset = TabularDataset(
+            x=np.hstack((x_val, np.ones((x_val.shape[0], 1)))).astype(np.float32),
+            z=z_val.astype(np.float32),
+            y=y_val.astype(np.float32),
+        )
+    else:
+        val_dataset = None
+
     train_dataset = TabularDataset(
         x=np.hstack((x_train, np.ones((x_train.shape[0], 1)))).astype(np.float32),
         z=z_train.astype(np.float32),
@@ -493,7 +510,7 @@ def prepare_dutch(base_path):
         y=y_test.astype(np.float32),
     )
 
-    return train_dataset, test_dataset
+    return train_dataset, test_dataset, val_dataset
 
 
 class LinearClassificationNet(nn.Module):
@@ -511,60 +528,92 @@ class LinearClassificationNet(nn.Module):
 
 
 class MixLoss(nn.Module):
-    def __init__(self, weight_cel=1.0, weight_mse=1.0, reduction="mean"):
+    def __init__(self, model_loss, unfairness_loss, reduction="mean"):
         super(MixLoss, self).__init__()
-        self.cel_loss = nn.CrossEntropyLoss()
-        self.cel_loss_2 = DisparityRegularizationLoss()
-        self.weight_cel = weight_cel
-        self.weight_mse = weight_mse
+        self.model_criterion = nn.CrossEntropyLoss()
+        self.unfairness_criterion = DisparityRegularizationLoss()
         self.reduction = reduction
 
     def forward(self, input, target):
-        """
-        Calculates a mixed loss combining NLLLoss and MSELoss.
-
-        Args:
-            input (torch.Tensor): The output from your model (e.g., m(input)).
-                                  This will be used for both NLLLoss and MSELoss.
-            target_nll (torch.Tensor): The target for NLLLoss (typically class labels).
-            target_mse (torch.Tensor): The target for MSELoss (typically continuous values).
-
-        Returns:
-            torch.Tensor: The weighted sum of CELoss and MSELoss.
-        """
         model_output = input[0]
         sensitive_value = input[1]
-        loss_cel = self.cel_loss(model_output, target)
-        loss_cel_2 = self.cel_loss_2(
+        lambda_regularization = input[2]
+
+        model_loss = self.model_criterion(model_output, target)
+        unfairness_loss = self.unfairness_criterion(
             sensitive_attribute_list=sensitive_value,
             device="cpu",
             predictions=model_output,
             possible_sensitive_attributes=[0, 1],
             possible_targets=[0, 1],
-        )  # You might need to adjust the input for MSE if it's not directly comparable to the NLL input.
+        )
+        return (1 - lambda_regularization) * model_loss + lambda_regularization * unfairness_loss
 
-        # return loss_cel
 
-        lambda_value = 0.6
-        total_loss = (1-lambda_value) * loss_cel + lambda_value * loss_cel_2
-        return total_loss
+def setup_wandb(project_name: str, run_name: str | None):
+    return wandb.init(project=project_name, name=run_name) if run_name else wandb.init(project=project_name)
+
+
+def check_input(args):
+    # check input for unfairness reduction parameters
+    if args.unfairness_reduction:
+        if args.regularization_lambda < 0 or args.regularization_lambda > 1:
+            raise ValueError("Lambda must be between 0 and 1.")
+        if args.fairness_metric not in ["disparity", "error_rate"]:
+            raise ValueError("Fairness metric must be either 'disparity' or 'error_rate'.")
+        if args.regularization_mode not in ["fixed", "tunable"]:
+            raise ValueError("Regularization model must be either 'fixed' or 'tunable'.")
+        if args.target is None:
+            raise ValueError("Target must be specified for unfairness reduction.")
+        if args.target < 0 or args.target > 1:
+            raise ValueError("Target must be between 0 and 1.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Puffle with specified configuration.")
+
+    # privacy parameters
     parser.add_argument("--epsilon", type=str, default=None)
     parser.add_argument("--noise_multiplier", type=float, default=0)
-    parser.add_argument(
-        "--max_grad_norm", type=float, default=10000000
-    )
+    parser.add_argument("--max_grad_norm", type=float, default=10000000)
+
+    # Training parameters
     parser.add_argument("--lr", type=float, required=True)
     parser.add_argument("--epochs", type=int, required=True)
+    parser.add_argument("--batch_size", type=int, required=True)
+    parser.add_argument("--optimizer", type=str, required=True)
+
+    # Wandb parameters
+    parser.add_argument("--wandb", type=bool, default=True)
+    parser.add_argument("--project_name", type=str, required=True)
+    parser.add_argument("--run_name", type=int)
+    parser.add_argument("--sweep", type=bool, default=False)
+
+    # Unfairness reduction parameters
+    parser.add_argument("--unfairness_reduction", type=float, default=False)
+    parser.add_argument("--regularization_lambda", type=float, default=0)
+    parser.add_argument("--fairness_metric", type=str, default="disparity")
+    parser.add_argument("--regularization_mode", type=str, default="fixed")
+    parser.add_argument("--target", type=float, default=None)
+
+    print("CIAO")
 
     args = parser.parse_args()
 
     seed_everything()
 
-    dutch_train, dutch_test = prepare_dutch("/raid/lcorbucci/data/dutch/")
+    check_input(args)
+    private_training = bool(args.noise_multiplier > 0 or args.epsilon is not None)
+
+    wandb_run = (
+        setup_wandb(
+            project_name=args.project_name,
+            run_name=args.run_name,
+        )
+        if args.wandb
+        else None
+    )
+    dutch_train, dutch_test, dutch_val = prepare_dutch("/raid/lcorbucci/data/dutch/", sweep=args.sweep)
 
     BATCH_SIZE = 256
 
@@ -584,39 +633,62 @@ if __name__ == "__main__":
         pin_memory=True,
     )
 
-    noise_multiplier = args.noise_multiplier
-    max_grad_norm = args.max_grad_norm
+    if dutch_val is not None:
+        val_loader = torch.utils.data.DataLoader(
+            dutch_val,
+            batch_size=BATCH_SIZE,
+            shuffle=False,
+            num_workers=0,
+            pin_memory=True,
+        )
+    else:
+        val_loader = None
+
+
+
     lr = args.lr
     epochs = args.epochs
     MAX_PHYSICAL_BATCH_SIZE = 1024
 
     privacy_engine = PrivacyEngine()
-    criterion = MixLoss()
+    criterion = MixLoss(
+        model_loss=nn.CrossEntropyLoss(),
+        unfairness_loss=DisparityRegularizationLoss(),
+    )
     model = LinearClassificationNet(input_size=11, output_size=2)
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0)
-    model_gc, optimizer_gc, criterion_gc, train_loader_gc = privacy_engine.make_private( 
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0) if args.optimizer == "sgd" else optim.Adam(model.parameters(), lr=lr)
+
+    model_gc, optimizer_gc, criterion_gc, train_loader_gc = privacy_engine.make_private(
         module=model,
         optimizer=optimizer,
         data_loader=train_loader,
-        noise_multiplier=noise_multiplier,
-        max_grad_norm=max_grad_norm,
+        noise_multiplier=args.noise_multiplier,
+        max_grad_norm=args.max_grad_norm,
         criterion=criterion,
         grad_sample_mode="ghost",
-        poisson_sampling=False,
+        poisson_sampling = True if private_training else False,
     )
 
+
     puffle_model = PUFFLEModel(
-        model=model_gc, 
+        model=model_gc,
         optimizer=optimizer_gc,
         criterion=criterion_gc,
-        device=torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda")
+        device=torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda"),
+        lambda_regularization=args.regularization_lambda,
+        wandb_run=wandb_run,
+        target=args.target,
     )
+
 
     puffle_model.train(
         train_loader=train_loader_gc,
         epochs=epochs,
-        val_loader=test_loader,
+        val_loader=val_loader if dutch_val is not None else test_loader,
         verbose=True,
         average_probabilities=None,
         max_physical_batch_size=MAX_PHYSICAL_BATCH_SIZE,
     )
+
+    if wandb_run:
+        wandb_run.finish() 
